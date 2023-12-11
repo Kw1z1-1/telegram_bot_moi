9448e95b166eaba0209256cd78356276b289a5a6
Kw1z1-1 <153484702+Kw1z1-1@users.noreply.github.com>
2023-12-11 09:39:28 +0300

    Update main.py tg bot

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import telebot
 from telebot import types
 import random
 
-bot = telebot.TeleBot("Your token")
+bot = telebot.TeleBot("")
 
 @bot.message_handler(commands=["start"])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add(types.KeyboardButton("Рандомное число"))
+    markup.add(types.KeyboardButton("Рандомный напиток"))
     bot.send_message(message.chat.id, "Выберите действие", reply_markup=markup)
 
 @bot.message_handler(commands=["hello"])
@@ -15,10 +16,13 @@ def hello(message):
     bot.send_message(message.chat.id, "Привет")
 
 
-@bot.message_handler(commands=["random"])
+@bot.message_handler(content_types=['text'])
 def randomize(message):
-    num = random.randint(1, 100)
-    bot.send_message(message.chat.id, str(num))
-
+    if message.text == 'Рандомное число':
+        num = random.randint(1, 100)
+        bot.send_message(message.chat.id, str(num))
+    if message.text == 'Рандомный напиток':
+        a = ['Кофе', 'Чай','квас', 'молоко', 'сироп оп оп оп']
+        bot.send_message(message.chat.id, random.choice(a))
 
 bot.polling(none_stop=True)
